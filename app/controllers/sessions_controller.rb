24794fb52910e2633bbf3f985b02221ec5e68d1b
takano-git <p.aaattt25@gmail.com>
2020-06-21 09:53:06 +0000

    Finish full-fledged-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ログイン後にユーザーズインデックスにリダイレクトします。
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to users_url
     else
       # レンダリングが終わっているページでフラッシュメッセージを表示
@@ -19,7 +19,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    # ログイン中の場合のみログアウト処理を実行します。
+    log_out if logged_in?
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
