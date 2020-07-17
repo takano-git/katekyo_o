c01f8b07d53c8d8a79a9477e745457401c35fecc
takano-git <p.aaattt25@gmail.com>
2020-07-17 22:36:01 +0000

    LINE Push Message test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,6 +1,6 @@
 class LinebotController < ApplicationController
   require 'line/bot'
-
+  # そのメソッドだけCSRF対策を無効にしたいというときはexceptで指定する
   protect_from_forgery :except => [:callback]
 
   def client
@@ -11,6 +11,7 @@ class LinebotController < ApplicationController
   end
 
   def callback
+    # jsonを読み込む
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
@@ -27,8 +28,14 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
           if event.message['text'].eql?('アンケート')
+            # コメントにしたコード
             # private内のtemplateメソッドを呼び出します。
-            client.reply_message(event['replyToken'], template)
+            # client.reply_message(event['replyToken'], template)
+            
+            # 試しに追加したコード
+            userid = event['source']['userId']  #userId取得
+            message = { type: 'text', text: 'プッシュ送信のテスト成功しました' }
+            client.push_message(userid, message) #push送信
           end
         end
       end
@@ -36,7 +43,18 @@ class LinebotController < ApplicationController
 
     head :ok
   end
-
+  
+  # 自作関数　ユーザーにメッセージを送る機能
+  # def send_message
+  #   message={
+  #   type: 'text',
+  #   text: '(送信したいメッセージ)'
+  #   }
+    
+    # user_id = uから始まるLINEの送信先識別子
+    # response = client.push_message(user_id, mesage)
+  # end
+  
   private
 
   def template
