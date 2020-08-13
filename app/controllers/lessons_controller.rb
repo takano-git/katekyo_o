bfac547bfc672d2c53a743b3334eb4f9ecd90b8b
takano-git <p.aaattt25@gmail.com>
2020-08-13 23:36:01 +0000

    lesson時間の新規登録じ、重複チェック追加

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -26,19 +26,33 @@ class LessonsController < ApplicationController
     # すでに登録されているLessonを取得
     start_data = "#{params[:lesson][:lesson_date]} #{params[:lesson][:start]}"
     finish_data = "#{params[:lesson][:lesson_date]} #{params[:lesson][:finish]}"
-    already_there_lessons = Lesson.where(lesson_date: params[:date])
-    # already_there_lessons.each do |atl|
-    # 比較開始日付 <= 対象終了日付 AND 比較終了日付 >= 対象開始日付
-        # (params[:lesson][:start]).to_datetime <= alt.finish AND (params[:lesson][:finish]).to_datetime  >= alt.finish
-    # end
+    already_there_lessons = Lesson.where(lesson_date: params[:lesson][:lesson_date], user_id:params[:lesson][:user_id])
+
+    comparison_start_data = "#{params[:lesson][:lesson_date]} #{params[:lesson][:start]}+0900".to_datetime
+    comparison_finish_data = "#{params[:lesson][:lesson_date]} #{params[:lesson][:finish]}+0900".to_datetime
     
-    
+    # 新規作成用オブジェクト
     @lesson = @user.lessons.new(start: start_data, finish: finish_data, user_id: params[:lesson][:user_id], lesson_date: params[:lesson][:lesson_date],status: 1)
+
+    already_there_lessons.each do |atl|
+      # comparison_start_data < atl.finish
+    # 比較開始日付 <= 対象終了日付 AND 比較終了日付 >= 対象開始日付
+      # if comparison_start_data < atl.finish AND comparison_finish_data > atl.finish
+      # if comparison_start_data < comparison_finish_data
+
+      if comparison_start_data < atl.finish && comparison_finish_data > atl.start
+       # 重複しているので保存せずに戻る
+        flash[:danger] = "Lesson時間が重複しています。重複しない時間帯を入力してください。"
+        redirect_to user_url @user  and return  # 家庭教師のusers#show
+      end
+    end
+    
+    
     if @lesson.save
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
-      flash[:danger] = "Lesson可能時間の登録に失敗しました。やり直して下さい。"
+      flash[:danger] = "Lesson可能時間の登録に失敗しました。"
       redirect_to user_url @user   # 家庭教師のusers#show
     end
   end
